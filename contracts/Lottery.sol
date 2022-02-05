17046e15680d27b6443cf8f3bcf0324512f582d7
akshay.shah <akshay.shah@globant.com>
2022-02-05 19:08:44 +0530

    Lottery updated to pay manager 10%

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -26,7 +26,9 @@ contract Lottery {
 
     function pickWinner() public restricted {
         require(players.length >= 3);
+        uint256 managerFee = (address(this).balance * 10) / 100; // manager fee is 10%    // winner prize is 90%
         uint256 index = random() % players.length;
+        manager.transfer(managerFee);
         players[index].transfer(address(this).balance);
         players = new address payable[](0);
     }
