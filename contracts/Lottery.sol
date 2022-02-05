c083f148c6bafe1f701a56e4e99d43f39e8141e0
akshay.shah <akshay.shah@globant.com>
2022-02-05 18:42:01 +0530

    Lottery contract update

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -1,38 +1,47 @@
 // SPDX-License-Identifier: GPL-3.0
- 
+
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Lottery {
     address payable[] public players;
-    address public manager;
+    address payable manager;
 
     constructor() {
-        manager = msg.sender;
+        manager = payable(msg.sender);
+        players.push(manager);
     }
 
     receive() external payable {
-        require(msg.value == 1 ether);
+        require(msg.value == 1 ether, "Require 1 ether to participate");
+        require(
+            manager != msg.sender,
+            "Manager cannot directly participate in the lottery"
+        );
         players.push(payable(msg.sender));
     }
 
-    function getBalance() public view restricted returns(uint) {
+    function getBalance() public view restricted returns (uint256) {
         return address(this).balance;
     }
 
     function pickWinner() public restricted {
         require(players.length >= 3);
-        uint index = random() % players.length;
+        uint256 index = random() % players.length;
         players[index].transfer(address(this).balance);
         players = new address payable[](0);
     }
 
-    function random() private view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.timestamp,players,block.difficulty)));
+    function random() private view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(block.timestamp, players, block.difficulty)
+                )
+            );
     }
 
-    modifier restricted {
+    modifier restricted() {
         require(msg.sender == manager);
         _;
     }
-
 }
