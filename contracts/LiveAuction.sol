68624efeb043e76804a4bdbd58e10fb99f82a62b
akshay.shah <akshay.shah@globant.com>
2022-02-07 19:06:05 +0530

    Comments added for unexplained integers.

--- contracts/LiveAuction.sol
+++ contracts/LiveAuction.sol
@@ -1,93 +1,115 @@
 // SPDX-License-Identifier: GPL-3.0
- 
+
 pragma solidity >=0.5.0 <0.9.0;
 
 contract Auction {
     address payable public owner;
-    uint public startblock;
-    uint public endBlock;
+    uint256 public startblock;
+    uint256 public endBlock;
 
-    enum State{Started, Running, Cancelled, Ended}
+    enum State {
+        Started,
+        Running,
+        Cancelled,
+        Ended
+    }
     State public auctionState;
 
-    mapping(address => uint) public bids;
-    
-    uint public highestBid;
+    mapping(address => uint256) public bids;
+
+    uint256 public highestBid;
     address payable public highestBidder;
 
     string public ifpsHash;
-    uint public increment;
+    uint256 public increment;
 
-    constructor(address auctionCreator){
+    constructor(address auctionCreator) {
         owner = payable(auctionCreator);
         startblock = block.number;
-        endBlock = startblock + 10;
+        endBlock = startblock + 40320; // 40320 is blocks mined in a week (current 1 block = 15 seconds)
         auctionState = State.Running;
         ifpsHash = "";
         increment = 1 ether;
     }
 
-    modifier restricted {
-        require(owner != msg.sender,"Owner cannot participate in the bidding");
+    modifier restricted() {
+        require(owner != msg.sender, "Owner cannot participate in the bidding");
         _;
     }
 
-    modifier afterStart {
-        require(block.number >= startblock, "Current block less than start block");
-        require(block.number <= endBlock,"Current block greater than end block");
+    modifier afterStart() {
+        require(
+            block.number >= startblock,
+            "Current block less than start block"
+        );
+        require(
+            block.number <= endBlock,
+            "Current block greater than end block"
+        );
         _;
     }
 
-    modifier requiresOwner {
+    modifier requiresOwner() {
         require(msg.sender == owner);
         _;
     }
 
-    function min(uint a, uint b) internal pure returns(uint) {
-        if(a<b){
+    function min(uint256 a, uint256 b) internal pure returns (uint256) {
+        if (a < b) {
             return a;
-        }else {
+        } else {
             return b;
         }
     }
 
-    function placeBid() public payable restricted afterStart { 
+    function placeBid() public payable restricted afterStart {
         require(auctionState == State.Running, "Auction State not running");
         require(msg.value >= 1 ether, "Minimum payable 1 ether");
 
-        uint currentBid = bids[msg.sender] + msg.value;
-        require(currentBid > highestBid, "Your bid is less than current highest bid");
+        uint256 currentBid = bids[msg.sender] + msg.value;
+        require(
+            currentBid > highestBid,
+            "Your bid is less than current highest bid"
+        );
 
         bids[msg.sender] = currentBid;
 
-        if(bids[msg.sender] <= highestBid) {
+        if (bids[msg.sender] <= highestBid) {
             highestBid = min(currentBid + 100, bids[highestBidder]);
         } else {
             highestBid = min(currentBid, bids[highestBidder] + increment);
             highestBidder = payable(msg.sender);
         }
-
     }
 
     function finalizeAuction() public payable {
-        require(auctionState == State.Cancelled || block.number > endBlock, "Auction is still running, wait for the auction to get over");
-        require(msg.sender == owner || bids[msg.sender] > 0, "You are neither a owner nor a bidder, you cannot finalize the auction");
+        require(
+            auctionState == State.Cancelled || block.number > endBlock,
+            "Auction is still running, wait for the auction to get over"
+        );
+        require(
+            msg.sender == owner || bids[msg.sender] > 0,
+            "You are neither a owner nor a bidder, you cannot finalize the auction"
+        );
 
         address payable recipient = payable(msg.sender);
-        uint value;
+        uint256 value;
 
         // auction cancelled
-        if(auctionState == State.Cancelled){ 
+        if (auctionState == State.Cancelled) {
             value = bids[recipient];
-        } else { // auction ended
+        } else {
+            // auction ended
             // Owner
-            if(recipient == owner) {
+            if (recipient == owner) {
                 value = highestBid;
-            }else {// bidder
+            } else {
+                // bidder
                 // highest bidder
-                if(recipient == highestBidder){
+                if (recipient == highestBidder) {
                     value = bids[recipient] - highestBid;
-                }else { // normal bidder
+                } else {
+                    // normal bidder
                     value = bids[recipient];
                 }
             }
@@ -101,20 +123,18 @@ contract Auction {
         auctionState = State.Cancelled;
     }
 
-    function contractBalance() public view requiresOwner returns(uint){
+    function contractBalance() public view requiresOwner returns (uint256) {
         return address(this).balance;
     }
-
 }
-
 
 contract DeployAuction {
     address public auctionContractOwner;
     Auction[] public auctionsList;
+
     constructor() {
         auctionContractOwner = msg.sender;
     }
-
 
     function deployAuction() public {
         Auction newAuction = new Auction(msg.sender);
