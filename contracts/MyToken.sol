2bac403f5536f3b461271d557aae36ae267606b6
akshay.shah <akshay.shah@globant.com>
2022-02-08 11:21:28 +0530

    ERC-20 TOKEN CREATED

---
+++ contracts/MyToken.sol
@@ -0,0 +1,115 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.5.0 <0.9.0;
+
+interface ERC20Interface {
+    function totalSupply() external view returns (uint256);
+
+    function balanceOf(address tokenOwner)
+        external
+        view
+        returns (uint256 balance);
+
+    function transfer(address to, uint256 tokens)
+        external
+        returns (bool success);
+
+    function allowance(address tokenOwner, address spender)
+        external
+        view
+        returns (uint256 remaining);
+
+    function approve(address spender, uint256 tokens)
+        external
+        returns (bool success);
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) external returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint256 tokens);
+    event Approval(
+        address indexed tokenOwner,
+        address indexed spender,
+        uint256 tokens
+    );
+}
+
+contract MyToken is ERC20Interface {
+    mapping(address => uint256) public balances;
+    mapping(address => mapping(address => uint256)) allowed;
+
+    string public name = "Akshay Shah Coin";
+    string public symbol = "ASC";
+    uint256 public override totalSupply;
+    uint256 public decimals;
+    address public founder;
+
+    constructor() {
+        totalSupply = 100000000;
+        founder = msg.sender;
+        balances[founder] = totalSupply;
+        decimals = 0;
+    }
+
+    function balanceOf(address tokenOwner)
+        public
+        view
+        override
+        returns (uint256 balance)
+    {
+        return balances[tokenOwner];
+    }
+
+    function transfer(address to, uint256 tokens)
+        public
+        override
+        returns (bool success)
+    {
+        require(balances[msg.sender] >= tokens);
+
+        balances[to] += tokens;
+        balances[msg.sender] -= tokens;
+        emit Transfer(msg.sender, to, tokens);
+        return true;
+    }
+
+    function allowance(address tokenOwner, address spender)
+        public
+        view
+        override
+        returns (uint256 remaining)
+    {
+        return allowed[tokenOwner][spender];
+    }
+
+    function approve(address spender, uint256 tokens)
+        public
+        override
+        returns (bool success)
+    {
+        require(balances[msg.sender] >= tokens);
+        require(tokens >= 0);
+
+        allowed[msg.sender][spender] = tokens;
+        emit Approval(msg.sender, spender, tokens);
+        return true;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 tokens
+    ) public override returns (bool success) {
+        require(allowed[from][to] >= tokens);
+        require(balances[from] >= tokens);
+
+        balances[from] -= tokens;
+        balances[to] += tokens;
+        allowed[from][to] -= tokens;
+
+        return true;
+    }
+}
